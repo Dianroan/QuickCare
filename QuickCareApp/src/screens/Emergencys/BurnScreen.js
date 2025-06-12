540946b38d385479a842feff3df9c45a9a64b3bc
Diego Rodriguez <diegorodriguez.mt@gmail.com>
2025-06-12 14:03:10 -0600

    More Images

--- QuickCareApp/src/screens/Emergencys/BurnScreen.js
+++ QuickCareApp/src/screens/Emergencys/BurnScreen.js
@@ -6,37 +6,43 @@ const steps = [
     {
         text: "Verifica que el entorno sea seguro antes de acercarte a la persona.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/Burn%2FBurnStep1.png?alt=media&token=ed28aa5a-804d-40fa-8a20-94284c93c49e",    
+            require("../../../assets/Burn/Burn1.png"),
     },
     {
         text: "Detén la causa de la quemadura (fuego, corriente eléctrica, químico, etc.) si es seguro hacerlo.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/Burn%2FBurnStep2.png?alt=media&token=801a2158-67bc-4653-8c79-2635ae9a9e3a",
+            require("../../../assets/Burn/Burn2.png"),
+
     },
     {
         text: "Enfría la zona afectada con agua limpia a temperatura ambiente durante al menos 10 minutos.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/Burn%2FBurnStep3.png?alt=media&token=46f8b989-27aa-4c08-8809-5ffb6b4c3be8",
+            require("../../../assets/Burn/Burn3.png"),
+
     },
     {
         text: "No apliques hielo, pasta de dientes ni remedios caseros sobre la quemadura.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/Burn%2FBurnStep4.png?alt=media&token=bfe0a7a9-e0eb-4403-8fcb-bcb91f8e97e7",
+            require("../../../assets/Burn/Burn4.png"),
+
     },
     {
         text: "Cubre la quemadura con un paño limpio o gasa estéril sin presionar demasiado.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/Burn%2FBurnStep5.png?alt=media&token=21de58f0-94d2-4f07-a9d2-cad3035b1fa3",
+            require("../../../assets/Burn/Burn5.png"),
+
     },
     {
         text: "Retira cuidadosamente anillos, pulseras o ropa cercana antes de que se hinche la zona, solo si no están pegados a la piel.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/Burn%2FBurnStep6.png?alt=media&token=bc3e7da2-5cf3-495b-934a-4e6ab149c197",
+            require("../../../assets/Burn//Burn6.png"),
+
     },
     {
         text: "Llama al 911 si la quemadura es extensa, afecta la cara, genitales o articulaciones, o si la persona tiene dificultad para respirar.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/Burn%2FBurnStep7.png?alt=media&token=3a4582d9-24d0-497b-83f4-d17e8c92b4d1",
+            require("../../../assets/Burn/Burn7.png"),
+
     },
 ];
 
--- QuickCareApp/src/screens/Emergencys/CardiacArrestScreen.js
+++ QuickCareApp/src/screens/Emergencys/CardiacArrestScreen.js
@@ -6,42 +6,42 @@ const steps = [
     {
         text: "Verifica que el entorno sea seguro antes de acercarte a la persona.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep1.png?alt=media&token=b2e7f7e7-f4ff-4e86-81e5-043dc53d1102",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
     {
         text: "Comprueba si la persona responde y respira normalmente. Si no lo hace, es una emergencia.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep2.png?alt=media&token=749e1a86-7d46-4c55-a61b-8e8a3b65fc2c",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
     {
         text: "Llama inmediatamente al 911 o pide a alguien más que lo haga.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep3.png?alt=media&token=165db812-b6a1-4f1d-8862-5d6d8c401ad9",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
     {
         text: "Inicia compresiones torácicas fuertes y rápidas en el centro del pecho (a un ritmo de 100-120 por minuto).",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep4.png?alt=media&token=3f78843e-3e5f-4701-9db6-6bc9b2798053",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
     {
         text: "Presiona con ambas manos una sobre otra, con los brazos rectos y el cuerpo alineado sobre el pecho.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep5.png?alt=media&token=40b987b3-98a3-4075-84e5-f7e9291908e2",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
     {
         text: "Continúa con las compresiones sin detenerte hasta que llegue ayuda o un desfibrilador (DEA).",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep6.png?alt=media&token=0d8e7b32-bc21-4d84-92ea-60449c0fa302",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
     {
         text: "Si hay un DEA disponible, enciéndelo y sigue sus instrucciones inmediatamente.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep7.png?alt=media&token=b90b1263-1964-4a6d-98b6-47318703fdd4",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
     {
         text: "No suspendas las compresiones a menos que la persona recupere la conciencia o lleguen los servicios de emergencia.",
         image:
-            "https://firebasestorage.googleapis.com/v0/b/quick-care-project.firebasestorage.app/o/CardiacArrest%2FCardiacStep8.png?alt=media&token=6d3e1d11-2d04-40e3-8017-5d168cdb40fd",
+            require("../../../assets/Bleeding/Bleeding6.png"),
     },
 ];
 
